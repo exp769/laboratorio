b3ff2c9a7787a5954230353a2b9b1327ba3bf891
expur <leotokuo@gmail.com>
2024-11-14 20:43:46 -0300

    Update main.py

--- FASTAPI/app/main.py
+++ FASTAPI/app/main.py
@@ -1,12 +1,12 @@
-from typing import union
-from fastapi import fastAPI
+from typing import Union
+from fastapi import FastAPI
 
-app = fastAPI()
+app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 @app.get("/livro/livro_id")
-def read_livro(livro_id: int, q:union[str, None] = None)
+def read_livro(livro_id: int, q:Union[str, None] = None)
     return {"item_id": item_id, "q": q}
