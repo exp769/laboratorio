663e007a3d82004cfc8d99b9f27ae0a78b4331f2
expur <leotokuo@gmail.com>
2024-11-14 20:07:53 -0300

    Add files via upload

---
+++ FASTAPI/app/main.py
@@ -0,0 +1,12 @@
+from typing import union
+from fastapi import fastAPI
+
+app = fastAPI()
+
+@app.get("/")
+def read_root():
+    return ("Hello": "World")
+
+@app.get("/livro/livro_id")
+def read_livro(livro_id: int, q:union[str, None] = None)
+    return ("item_id": item_id, "q": q)
