9506728ec99bc2e6d44d017f0dcef4aa04153b17
expur <leotokuo@gmail.com>
2024-11-14 20:19:59 -0300

    Update main.py

--- FASTAPI/app/main.py
+++ FASTAPI/app/main.py
@@ -5,8 +5,8 @@ app = fastAPI()
 
 @app.get("/")
 def read_root():
-    return ("Hello": "World")
+    return {"Hello": "World"}
 
 @app.get("/livro/livro_id")
 def read_livro(livro_id: int, q:union[str, None] = None)
-    return ("item_id": item_id, "q": q)
+    return {"item_id": item_id, "q": q}
